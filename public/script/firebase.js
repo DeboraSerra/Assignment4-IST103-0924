561dbee6650dd7ddb634db97071dbc9e44d26e0b
Débora Serra <debora.r.serra@gmail.com>
2024-10-05 11:12:10 -0700

    feat: add signin

---
+++ public/script/firebase.js
@@ -0,0 +1,21 @@
+/*
+  CCTB Website Development
+  IST105
+  Oct 2024
+  Description: This is a simple login website where students are asked to 
+  implement Social Network Login with Firebase
+*/
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyATxV6KFqOx3CtcAqisw0AYCaKH0nj2uag",
+  authDomain: "portfoliov1deboraserra.firebaseapp.com",
+  projectId: "portfoliov1deboraserra",
+  storageBucket: "portfoliov1deboraserra.appspot.com",
+  messagingSenderId: "431110764957",
+  appId: "1:431110764957:web:fb38e18a6554e51d26d774"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
---
+++ public/script/home.js
@@ -0,0 +1,65 @@
+const DASHBOARD = "listprojects.html"
+
+const signInGoogle = async () => {
+  let provider = new firebase.auth.GoogleAuthProvider();
+
+  provider.addScope("email");
+
+  try {
+    const userCredential = await firebase.auth().signInWithPopup(provider);
+    console.log("Logging successfully", userCredential.user);
+    location.href = DASHBOARD;
+  } catch (error) {
+    let errorCode = error.code;
+    let errorMessage = error.message;
+    console.log("Logging fail", errorMessage, errorCode);
+  }
+};
+
+const signIn = async (e) => {
+  e.preventDefault();
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+
+  try {
+    const userCredential = await firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password);
+    let user = userCredential.user;
+    console.log("Logging successfully", user);
+    alert("Logging successfully");
+    location.href = DASHBOARD;
+  } catch (e) {
+    let errorCode = error.code;
+    let errorMessage = error.message;
+    console.log("Logging fail", errorMessage, errorCode);
+  }
+};
+
+const signUp = async () => {
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+
+  try {
+    const userCredential = await firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password);
+    let user = userCredential.user;
+    console.log("Account created", user);
+    alert("Account created");
+    location.href = DASHBOARD;
+  } catch (error) {
+    let errorCode = error.code;
+    let errorMessage = error.message;
+    console.log("Logging fail", errorMessage, errorCode);
+  }
+};
+
+window.onload = () => {
+  document.querySelector('form').addEventListener('submit', signIn)
+  document.getElementById("sign-in").addEventListener("click", signIn);
+  document
+    .getElementById("sign-google")
+    .addEventListener("click", signInGoogle);
+  document.getElementById("sign-up").addEventListener("click", signUp);
+};
